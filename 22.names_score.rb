72af78dc7cbf764ae0f734ca4b8bf66efd374e78
Santu Chall <santuchal@gmail.com>
2018-06-05 13:18:30 +0530

    22 Complete

--- 22.names_score.rb
+++ 22.names_score.rb
@@ -5,3 +5,24 @@
 # For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
 
 # What is the total of all the name scores in the file?
+
+def convert_to_ascii(word)
+  total = 0
+  word.each_byte do |i|
+    total += i - 64
+  end
+  return total
+end
+
+file  = File.new("names.txt", "r")
+names = eval("[" + file.gets + "]").sort
+total = 0
+
+file.close
+
+names.each_with_index do |name, i|
+  total =  total + convert_to_ascii(name) * (i + 1)
+end
+
+puts "Total is #{total}"
+	
